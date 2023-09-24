6fe7a0260fe620e0dc59cd9adf84ccfcc270d664
usman <usman-hashmi@hotmail.com>
2023-09-24 19:31:47 +0200

    mongodb, kontakt, dockerstuff

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 const port = 3000;
 
+const mongoDBConnection =
+    process.env.MONGODB_CONNECTION ||
+    'mongodb://root:example@localhost:27017/?authMechanism=DEFAULT';
+
+    mongoose.connect(mongoDBConnection, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+const Kontakt = mongoose.model('Kontakt',{
+    email: String,
+    betreff: String,
+    nachricht: String
+});
+
 app.use(express.static(__dirname));
+
+app.post('/kontakt', async (req, res) => {
+    try {
+      const kontakt = new Kontakt({
+        email: req.body.email,
+        betreff: req.body.betreff,
+        nachricht: req.body.nachricht
+      });
+      await kontakt.save();
+      
+      console.log('Kontaktdaten wurden erfolgreich in die Datenbank eingefügt.');
+      res.redirect('/subsites/bestatigung.html');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Fehler beim Speichern der Kontaktdaten.');
+    }
+  });
 
 app.get('/', (req, res) => {
   res.send('Willkommen auf meiner Webseite!');
